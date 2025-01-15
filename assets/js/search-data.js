573b29f60dda34486ccbfbf05460fcfacf1ecfa9
utksi <utkarsh.singh@liu.se>
2025-01-15 21:57:32 +0000

    Deploying to gh-pages from @ utksi/utksi.github.io@3130c4c77d28ff9ae965b2fb49888a2f5767d4ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -206,14 +206,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%75%74%6B%61%72%73%68.%73%69%6E%67%68@%6C%69%75.%73%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/utksi", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-5150-6189", "_blank");
         },
       },{
         id: 'social-rss',
@@ -223,18 +230,18 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-work',
+        title: 'Work',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://liu.se/en/employee/utksi60", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/RomulanAle", "_blank");
         },
       },{
       id: 'light-theme',
