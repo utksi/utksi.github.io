ed04e6b05800743df877211439d02ce1b572d614
utksi <utkarsh.singh@liu.se>
2025-01-15 03:17:01 +0000

    Deploying to gh-pages from @ utksi/utksi.github.io@bc8a37827e3ffd74e7e42de01b7fc32ac48a4159 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,15 +79,26 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+            },{id: "post-stuff-i-read-dark-states-of-electrons-in-a-quantum-system-with-two-pairs-of-sublattices",
       
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: "[Stuff I read] Dark states of electrons in a quantum system with two...",
       
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+      description: "A high-level summary of main ideas from Chung et al.",
       section: "Posts",
       handler: () => {
         
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
+          window.location.href = "/blog/2024/dark_states/";
+        
+      },
+    },{id: "post-stuff-i-read-phonon-induced-renormalization-of-exchange-interactions-in-two-dimensional-magnets",
+      
+        title: "[Stuff I read] Phonon induced renormalization of exchange interactions in two-dimensional magnets",
+      
+      description: "A high-level summary of main ideas from Badrtdinov et al.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/renorm/";
         
       },
     },{id: "post-cooperative-graph-neural-networks",
@@ -105,11 +116,22 @@ ninja.data = [{
       
         title: "E(3) - equivariant GNN with Learnable Activation Functions on Edges",
       
-      description: "Some ideas about KAN based GNNs beyond just stacking layers",
+      description: "Some ideas about KAN-based GNNs beyond just stacking layers",
       section: "Posts",
       handler: () => {
         
           window.location.href = "/blog/2024/gnn_kan/";
+        
+      },
+    },{id: "post-stuff-i-read-kolmogorov-arnold-networks",
+      
+        title: "[Stuff I read] Kolmogorov Arnold Networks",
+      
+      description: "A very short summary of main ideas from Liu et al.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/kan/";
         
       },
     },{id: "post-mace-message-passing-ace",
@@ -121,17 +143,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2023/mace/";
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
     },{id: "news-a-simple-inline-announcement",
